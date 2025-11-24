1a9faf0815c260a14fe85e4bc98a26f5a9994e38
Artur Radchenko <kisharchi@gmail.com>
2025-11-24 21:35:51 +0100

    add scrolling blocks

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,8 @@ import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
 import accordion from './modules/accordion';
 import burger from './modules/burger';
+// import scrolling from './modules/scrolling';
+import scrollingRAF from './modules/scrollingRAF';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict'
@@ -26,5 +28,7 @@ window.addEventListener('DOMContentLoaded', () => {
   pictureSize('.sizes-block');
   accordion('.accordion-heading');
   burger('.burger-menu', '.burger');
+  // scrolling('.pageup');
+  scrollingRAF('.pageup');
 
 });
